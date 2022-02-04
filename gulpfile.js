c146b80834667231c6c9091ad5c11455ac6fbf94
ternova <ternova.ekaterina@gmail.com>
2022-02-04 14:51:03 +0200

    added product-list-1, product-list-2, product-detalis

--- gulpfile.js
+++ gulpfile.js
@@ -37,6 +37,7 @@ function scripts() {
     'node_modules/mixitup/dist/mixitup.js',
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
     'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+    'node_modules/rateyo/src/jquery.rateyo.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
